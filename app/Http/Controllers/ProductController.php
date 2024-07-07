0dac6e36a8d9f43dc5daa369272366738b58dc6b
Jude <mpoyojude0@gmail.com>
2024-07-07 10:19:41 +0200

    before adding filament-shield

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
@@ -21,7 +22,6 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
